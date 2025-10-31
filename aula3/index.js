7ba4b47a100690f0511bea68008af91b71963a02
valmirdev <valmir473355@gmail.com>
2025-10-31 17:21:30 -0300

    strings e numbers

---
+++ aula3/index.js
@@ -0,0 +1,6 @@
+console.log("Valmir 'Augusto'"); // String = Texto
+console.log('Valmir "Augusto"'); // Number = Número
+console.log(`'Valmir' "Augusto"`);
+
+
+console.log(35, 15.85, 'Valmir Augusto de novo');
